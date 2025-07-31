4cd8bc9b53a9e9a93232c68b5e86b6a871c1c570
muhamedhamedvl <mh1191128@gmail.com>
2025-07-31 17:59:53 +0300

    ExceptionMiddleware , ApiExceptions

---
+++ WebApiEcomm.API/Helper/ApiExceptions.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiEcomm.API.Helper
+{
+    public class ApiExceptions : ResponseApi
+    {
+        public ApiExceptions(string message, string details, int statusCode = 500) : base(statusCode, message)
+        {
+            Details = details;
+        }
+        public string Details { get; set; } = string.Empty;
+    }
+}
---
+++ WebApiEcomm.API/Middleware/ExceptionsMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace WebApiEcomm.API.Middleware
+{
+    public class ExceptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var response = new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = "An unexpected error occurred.",
+                    Details = ex.Message 
+                };
+                var jsonResponse = System.Text.Json.JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(jsonResponse);
+            }
+        }
+    }
+}
