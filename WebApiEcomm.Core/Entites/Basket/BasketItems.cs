3b9d51a3b2b2cfb22b3a8575df2f26f605913f3f
muhamedhamedvl <mh1191128@gmail.com>
2025-08-06 22:43:58 +0300

    Entites =>  Basket , Customer Basket

---
+++ WebApiEcomm.Core/Entites/Basket/BasketItems.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiEcomm.Core.Entites.Basket
+{
+    public class BasketItems
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+
+    }
+}
---
+++ WebApiEcomm.Core/Entites/Basket/CustomerBasket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiEcomm.Core.Entites.Basket
+{
+    public class CustomerBasket
+    {
+        public CustomerBasket()
+        {
+            
+        }
+        public CustomerBasket(int Id)
+        { 
+            this.Id = Id;
+        }
+        public int Id { get; set; }
+        public List<BasketItems> basketItems { get; set; } = new List<BasketItems>();
+    }
+}
