9e9fd968ca562b0a74568ea97d9d5fdf82980662
muhamedhamedvl <mh1191128@gmail.com>
2025-08-07 18:06:34 +0300

    BasketController

---
+++ WebApiEcomm.API/Controllers/BasketController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiEcomm.API.Helper;
+using WebApiEcomm.Core.Entites.Basket;
+using WebApiEcomm.Core.Interfaces.IUnitOfWork;
+
+namespace WebApiEcomm.API.Controllers
+{
+
+    public class BasketController : BaseController
+    {
+        public BasketController(IUnitOfWork work, IMapper mapper) : base(work, mapper)
+        {
+        }
+        [HttpGet("Get-Basket-Item/{id}")]
+        public async Task<IActionResult> GetBasket(string id)
+        {
+            var res = await _work.CustomerBasketRepository.GetCustomerBasketAsync(id);
+            return Ok(res);
+        }
+        [HttpPut("update-Basket")]
+        public async Task<IActionResult> Update(CustomerBasket customerBasket)
+        {
+            var basket = await _work.CustomerBasketRepository.UpdateCustomerBasketAsync(customerBasket);
+            return Ok(basket);
+        }
+        [HttpDelete("delete-basket-item/{id}")]
+        public async Task<IActionResult> DeleteById(string id)
+        {
+            var res = await _work.CustomerBasketRepository.DeleteCustomerBasketAsync(id);
+            return res ? Ok(new ResponseApi(200 , "Basket Deleted")) : BadRequest(new ResponseApi(400));
+        }
+    }
+}
