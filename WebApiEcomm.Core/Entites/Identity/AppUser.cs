0e55838db9d41fc8bfebe8c79bdbe261049718be
muhamedhamedvl <mh1191128@gmail.com>
2025-08-08 21:06:17 +0300

    AppUser

--- WebApiEcomm.Core/Entites/Identity/AppUser.cs
+++ WebApiEcomm.Core/Entites/Identity/AppUser.cs
@@ -10,6 +10,11 @@ namespace WebApiEcomm.Core.Entites.Identity
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
-        public Address address { get; set; }    
+        public Address address { get; set; }
+        public string PhoneNumber { get; init; }
+        public string Address { get; init; }
+        public string City { get; init; }
+        public string State { get; init; }
+        public string ZipCode { get; init; }
     }
 }
