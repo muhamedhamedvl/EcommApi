558f39d777ff6057ec43bc54413edb32e66e9911
muhamedhamedvl <mh1191128@gmail.com>
2025-08-11 18:15:50 +0300

    RegisterDto

--- WebApiEcomm.Core/Entites/Dtos/RegisterDto.cs
+++ WebApiEcomm.Core/Entites/Dtos/RegisterDto.cs
@@ -6,16 +6,8 @@ using System.Threading.Tasks;
 
 namespace WebApiEcomm.Core.Entites.Dtos
 {
-    public record RegisterDto
+    public record RegisterDto : LoginDto
     {
         public string UserName { get; init; }
-        public string Email { get; init; }
-        public string Password { get; init; }
-        public string ConfirmPassword { get; init; }
-        public string PhoneNumber { get; init; }
-        public string Address { get; init; }
-        public string City { get; init; }
-        public string State { get; init; }
-        public string ZipCode { get; init; }
     }
 }
