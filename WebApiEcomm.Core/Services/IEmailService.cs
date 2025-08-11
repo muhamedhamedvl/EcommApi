cc33ee2a9de7398d32c2c57a2f32ec5a2647df46
muhamedhamedvl <mh1191128@gmail.com>
2025-08-11 18:16:25 +0300

    implement sendmail in IEmailService

---
+++ WebApiEcomm.Core/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiEcomm.Core.Entites.Dtos;
+
+namespace WebApiEcomm.Core.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmail(EmailDto emailDto);
+    }
+}
