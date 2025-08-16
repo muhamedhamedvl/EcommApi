02b4f1e705a42eead9a9edd692b9fd6daf4f2ec5
muhamedhamedvl <mh1191128@gmail.com>
2025-08-17 01:45:35 +0300

    PaymentController

---
+++ WebApiEcomm.API/Controllers/PaymentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using WebApiEcomm.Core.Entites.Basket;
+using WebApiEcomm.Core.Services;
+
+namespace WebApiEcomm.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService paymentService;
+        public PaymentController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+        [HttpPost("Create/{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> Create(string basketId , int deliveryid )
+        {
+            return await paymentService.CreateOrUpdatePaymentAsync(basketId , deliveryid);
+        }
+    }
+}
