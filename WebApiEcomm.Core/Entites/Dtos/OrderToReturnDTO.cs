b82ec6d7e23be9e7e6e7e4ba3327460bb727ce90
muhamedhamedvl <mh1191128@gmail.com>
2025-08-17 02:27:48 +0300

    OrderToReturnDTO

---
+++ WebApiEcomm.Core/Entites/Dtos/OrderToReturnDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiEcomm.Core.Entites.Order;
+
+namespace WebApiEcomm.Core.Entites.Dtos
+{
+    public record OrderToReturnDTO
+    {
+        public int Id { get; set; }
+        public string BuyerEmail { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+        public DateTime OrderDate { get; set; }
+        public ShippingAddress shippingAddress { get; set; }
+
+        public IReadOnlyList<OrderItemDto> orderItems { get; set; }
+        public string deliveryMethod { get; set; }
+
+
+        public string status { get; set; }
+    }
+}
