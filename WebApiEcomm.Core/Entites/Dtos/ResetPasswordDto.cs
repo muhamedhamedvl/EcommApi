b45961874592509d395321fa0e1f31062ace11cc
muhamedhamedvl <mh1191128@gmail.com>
2025-08-16 16:30:41 +0300

    ResetPasswordDto Added For Authentecation perpose

---
+++ WebApiEcomm.Core/Entites/Dtos/ResetPasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiEcomm.Core.Entites.Dtos
+{
+    public record ResetPasswordDto : LoginDto
+    {
+        public string Token { get; init; }
+    }
+}
